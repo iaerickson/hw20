a00b84a6098828bda3f61fd5f07aa2f24e0d4851
Ian <ierickson25@gmail.com>
2020-05-15 22:04:32 -0500

    connected server with skeleton of react app

--- best-buy-product-finder/src/App.js
+++ best-buy-product-finder/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<header className='App-header'></header>
+				<Switch>
+					<Route exact path='/' component={Home} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
+
+//Map out what calls are being made in each page
+//Home page with search
+//Top products
+//Shopping Cart Page
+//CRUD stuff in shopping cart
---
+++ best-buy-product-finder/src/pages/Home.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+
+function Home() {
+	const [homepage, setHomepage] = useState([]);
+	const [formObject, setFormObject] = useState({});
+}
